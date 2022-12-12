e822f637d22fba2fd446614816f95db78b5cab14
jack <kcajrehcaer@proton.me>
2022-12-12 05:05:57 +0000

    updates

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 g++ main.cpp phieu_DK_KTX/phieu_dk_ktx.cpp phieu_DK_KTX/ds_phieu_dk_ktx.cpp \
-phong/phong.cpp phong/Dsphong.cpp \
-day/Day.cpp day/DsDay.cpp \
-sinhvien/SV.cpp sinhvien/DsSV.cpp \
-lop/Lop.cpp lop/DSLop.cpp 
+    phong/phong.cpp phong/Dsphong.cpp \
+    day/Day.cpp day/DsDay.cpp \
+    sinhvien/SV.cpp sinhvien/DsSV.cpp \
+    lop/Lop.cpp lop/DSLop.cpp
